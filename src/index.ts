27e302be4b573f9b2adb605ef803ab1daa3b763a
James Cleveland <jc@blit.cc>
2018-01-31 12:34:23 +0000

    add files

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Context } from 'koa'
+import * as yaml from 'js-yaml'
+import * as msgpack from 'msgpack-lite'
+
+export const accept = () => async (ctx: Context, next: () => void) => {
+    await next()
+    const accept: string = ctx.headers.accept || 'application/json'
+    if (typeof ctx.body === 'string') return
+    const safeBody = JSON.parse(JSON.stringify(ctx.body))
+    if (accept.includes('application/yaml')) {
+        ctx.type = 'application/yaml'
+        ctx.body = `${yaml.safeDump(safeBody)}\n`
+    } else if (accept.includes('application/x-msgpack')) {
+        ctx.type = 'application/x-msgpack'
+        ctx.body = msgpack.encode(safeBody)
+    }
+}
