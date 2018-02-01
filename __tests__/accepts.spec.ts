1dc5ce7739fc4c254f337fd746f8743ec1841abf
James Cleveland <jc@blit.cc>
2018-02-01 10:59:08 +0000

    Using accept lib to respect q weights and parse properly

--- __tests__/accepts.spec.ts
+++ __tests__/accepts.spec.ts
@@ -5,13 +5,19 @@ import * as msgpack from 'msgpack-lite'
 const nxt = () => null
 
 describe('accepts', () => {
-    it('block', () => {
-        console.log('blah')
-        expect(1).toEqual(1)
-    })
+
     it('should not do anything for a json request', async () => {
         const ctx: any = {
             headers: { accept: 'application/json' },
+            body: { hi: 'ho' },
+        }
+        await accepts()(ctx, nxt)
+        expect(ctx.body).toMatchObject({ hi: 'ho' })
+    })
+
+    it('should not do anything for a */* request', async () => {
+        const ctx: any = {
+            headers: {},
             body: { hi: 'ho' },
         }
         await accepts()(ctx, nxt)
@@ -37,7 +43,7 @@ describe('accepts', () => {
         expect(ctx.body).toEqual(yaml.safeDump({ hi: 'ho' }))
     })
 
-    it('should encode msgpac', async () => {
+    it('should encode msgpack', async () => {
         const ctx: any = {
             headers: { accept: 'application/x-msgpack' },
             body: { hi: 'ho' },
@@ -45,6 +51,46 @@ describe('accepts', () => {
         await accepts()(ctx, nxt)
         expect(ctx.type).toEqual('application/x-msgpack')
         expect(ctx.body).toEqual(msgpack.encode({ hi: 'ho' }))
+    })
+
+    it('should respect msgpack priority', async () => {
+        const ctx: any = {
+            headers: { accept: 'application/yaml;q=0.1, application/x-msgpack;q=1.0' },
+            body: { hi: 'ho' },
+        }
+        await accepts()(ctx, nxt)
+        expect(ctx.type).toEqual('application/x-msgpack')
+        expect(ctx.body).toEqual(msgpack.encode({ hi: 'ho' }))
+    })
+
+    it('should respect yaml priority', async () => {
+        const ctx: any = {
+            headers: { accept: 'application/x-msgpack;q=0.1, application/yaml;q=1.0' },
+            body: { hi: 'ho' },
+        }
+        await accepts()(ctx, nxt)
+        expect(ctx.type).toEqual('application/yaml')
+        expect(ctx.body).toEqual(yaml.safeDump({ hi: 'ho' }))
+    })
+
+    it('should respect order priority', async () => {
+        const ctx: any = {
+            headers: { accept: 'application/x-msgpack;q=1.0, application/yaml;q=1.0' },
+            body: { hi: 'ho' },
+        }
+        await accepts()(ctx, nxt)
+        expect(ctx.type).toEqual('application/x-msgpack')
+        expect(ctx.body).toEqual(msgpack.encode({ hi: 'ho' }))
+    })
+
+    it('should respect order priority', async () => {
+        const ctx: any = {
+            headers: { accept: 'application/yaml;q=1.0, application/x-msgpack;q=1.0,' },
+            body: { hi: 'ho' },
+        }
+        await accepts()(ctx, nxt)
+        expect(ctx.type).toEqual('application/yaml')
+        expect(ctx.body).toEqual(yaml.safeDump({ hi: 'ho' }))
     })
 
 })
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import { Context } from 'koa'
 import * as yaml from 'js-yaml'
 import * as msgpack from 'msgpack-lite'
+import { mediaTypes } from 'accept'
 
-export const accepts = () => async (ctx: Context, next: () => void) => {
+export const accepts = () => async (ctx: Context, next: () => void): Promise<void> => {
     await next()
-    const accept: string = ctx.headers.accept || 'application/json'
+
     if (typeof ctx.body === 'string') return
+
+    // Parse using accept lib to process the weightings
+    const accept = mediaTypes(ctx.headers.accept)
+
+    // Remove "undefined" values
     const safeBody = JSON.parse(JSON.stringify(ctx.body))
-    if (accept.includes('application/yaml')) {
-        ctx.type = 'application/yaml'
-        ctx.body = yaml.safeDump(safeBody)
-    } else if (accept.includes('application/x-msgpack')) {
-        ctx.type = 'application/x-msgpack'
-        ctx.body = msgpack.encode(safeBody)
+
+    for (let idx = 0; idx < accept.length; idx++) {
+        const mediaType = accept[idx]
+
+        // YAML
+        if (mediaType === 'application/yaml') {
+            ctx.type = 'application/yaml'
+            ctx.body = yaml.safeDump(safeBody)
+            break
+        }
+
+        // MessagePack
+        if (mediaType === 'application/x-msgpack') {
+            ctx.type = 'application/x-msgpack'
+            ctx.body = msgpack.encode(safeBody)
+            break
+        }
+
     }
 }
